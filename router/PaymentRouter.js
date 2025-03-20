430312a56b91b9bfdaf8c189da23e2a3df3a8c14
Your Name <your.email@example.com>
2025-03-20 10:18:16 +0530

    Refund Router Added

--- router/PaymentRouter.js
+++ router/PaymentRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, verifyPayment } = require('../Controller/User/PaymentController');
+const { createOrder, verifyPayment, cancelAndRefundOrder } = require('../Controller/User/PaymentController');
 
 router.post("/orders", createOrder);
 router.post("/verify", verifyPayment);
+router.post("/cancel/:orderId",cancelAndRefundOrder)
 
 
 module.exports = router;
